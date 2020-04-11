6bda2b0f8322b08c82dc444f9d947e20e859bea6
Eric <x16.tux@gmail.com>
2020-04-10 20:58:02 -0400

    Moved App props, added build.gradle driver

--- src/main/java/com/ericulicny/oakshire/OakshireServiceApplication.java
+++ src/main/java/com/ericulicny/oakshire/OakshireServiceApplication.java
@@ -2,8 +2,10 @@ package com.ericulicny.oakshire;
 
 import org.springframework.boot.SpringApplication;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -17,5 +19,5 @@ public class OakshireServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(OakshireServiceApplication.class, args);
 	}
-	
+
 }
